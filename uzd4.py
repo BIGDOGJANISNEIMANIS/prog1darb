420b0344ef239fc745ca8a26c15e44cca4cf4e5c
BIGDOGJANISNEIMANIS <149487026+BIGDOGJANISNEIMANIS@users.noreply.github.com>
2024-12-18 15:52:18 +0200

    Create uzd4.py

---
+++ uzd4.py
@@ -0,0 +1,12 @@
+direction = str(input("kadu virzienu tu izvēlies? "))
+
+if direction == "w":
+    print("tu ej uz prieksu")
+elif direction == "a":
+    print("tu ej pa labi")
+elif direction == "s":
+    print("tu ej pa kreisi")
+elif direction == "d":
+    print("tu ej uz atpakalu")
+else:
+    print("kluda!")
