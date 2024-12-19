2bf88780e3bf987f7c13ad910e12712e44013914
BIGDOGJANISNEIMANIS <149487026+BIGDOGJANISNEIMANIS@users.noreply.github.com>
2024-12-19 16:30:44 +0200

    Create uzd13.py

---
+++ uzd13.py
@@ -0,0 +1,4 @@
+a = float(input("Ievadiet pirmo skaitli: "))
+b = float(input("Ievadiet otro skaitli: "))
+c = float(input("Ievadiet trešo skaitli: "))
+print("Lielākais skaitlis ir:", max(a, b, c))
