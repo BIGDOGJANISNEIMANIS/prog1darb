d97dfa6e1b1e58517509cd5bc758cebf07f90f4a
BIGDOGJANISNEIMANIS <149487026+BIGDOGJANISNEIMANIS@users.noreply.github.com>
2024-12-19 16:30:46 +0200

    Create uzd12.py

---
+++ uzd12.py
@@ -0,0 +1,9 @@
+n = int(input("Ievadiet skaitli n (n > 1): "))
+if n > 1:
+    i = 0
+    while i < 1:
+        result = 2 * n
+        i += 1
+    print(f"Skaitlis 2n ir: {result}")
+else:
+    print("Ievadiet skaitli n, kas ir lielāks par 1.")
