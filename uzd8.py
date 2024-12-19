55c48ff15524b44f0014549a35c7481681705e7d
BIGDOGJANISNEIMANIS <149487026+BIGDOGJANISNEIMANIS@users.noreply.github.com>
2024-12-19 16:30:30 +0200

    Create uzd8.py

---
+++ uzd8.py
@@ -0,0 +1,7 @@
+answer = str(input("tu gribi, lai programma atkārtojās "))
+if answer == "y":
+    answer = str(input("tu gribi, lai programma atkārtojās "))
+elif answer == "n":
+    print("okay atā")
+else:
+    print("ko?")
